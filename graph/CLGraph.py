fd4d454f4d1df0f419b126abae5ddc83691cc021
Teal <pershint@github.com>
2017-06-07 20:15:30 -0400

    working on prettifying the CLGraph output

--- graph/CLGraph.py
+++ graph/CLGraph.py
@@ -2,12 +2,29 @@ import numpy as np
 import ROOT
 
 def CLGraph(data):
-    bs,b1 = np.sort(data), data*0+1
+    bs,b1 = np.sort(np.array(data)), np.array(data)*0+1
+    x68 = bs[int(len(bs)*.683)]
+    x95 = bs[int(len(bs)*0.95)]
     x,y = [],[]
     xOld,cnt = 0,0
     tot = float(np.sum(b1))
     hc = ROOT.TH1D('hc','hc',np.max(data),0+0.5,np.max(data)+0.5)
     gc = ROOT.TGraph()
+    CLLines = ["l68":ROOT.TLine(),"l95":ROOT.TLine()]
+    for key in CLLines:
+        CLLines[key].SetY1(0.0)
+        CLLines[key].SetY2(1e6)
+
+    l68.SetX1(100.)
+    l68.SetX2(100.01)
+    l68.SetY1(0.)
+    l68.SetY2(100.)
+
+    l68 = ROOT.TLine()
+    l68.SetX1(100.)
+    l68.SetX2(100.01)
+    l68.SetY1(0.)
+    l68.SetY2(100.)
 
     for i,idx in enumerate(bs):
         print 'a',idx,i
@@ -20,7 +37,7 @@ def CLGraph(data):
         xOld = idx
 
     gc.Draw('al')
-    return gc
+    return gc, l68, l95
 
 if __name__ == "__main__":
     print("AAH")
