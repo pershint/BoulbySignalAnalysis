c0d30d00fd87eb57a185f788be560907a95f21fd
Teal <pershint@github.com>
2018-09-04 00:57:47 -0400

    New graphing functions added for the Forward-Backward algorithm plots

--- graph/KFGraphs.py
+++ graph/KFGraphs.py
@@ -3,21 +3,21 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 #Graphs that plot results from the Kalman Filter Analysis
 
-def Rebin_PLPH(PL_dist, PH_dist, Exp_days, daysperbin=3):
+def Rebin_PLPHs(Prob_array, Exp_days, daysperbin=3):
     '''Re-bins the probability distributions and averages the days in each bin'''
-    PL_rebinned = []
-    PH_rebinned = []
-    this_day = 0
-    while (this_day + daysperbin) < len(PL_dist/float(daysperbin)):#for day in xrange(len(Exp_days)):
-        PH_rebinned.append(np.sum(PH_dist[this_day:this_day+daysperbin])/float(daysperbin))
-        PL_rebinned.append(np.sum(PL_dist[this_day:this_day+daysperbin])/float(daysperbin))
-        this_day+=daysperbin
-    PH_rebinned = np.array(PH_rebinned)
-    PL_rebinned = np.array(PL_rebinned)
-    if len(Exp_days) > len(PH_rebinned):
-        Exp_days = list(Exp_days) 
-        Exp_days.pop(len(Exp_days)-1)
-    return PL_rebinned, PH_rebinned, np.array(Exp_days)
+    Probs_rebinned = []
+    for j,parr in enumerate(Prob_array):
+        this_day = 0
+        parr_rebinned = []
+        while ((this_day + daysperbin) < (len(parr))):#for day in xrange(len(Exp_days)):
+            parr_rebinned.append(np.sum(parr[this_day:this_day+daysperbin])/float(daysperbin))
+            this_day+=daysperbin
+        P_rebinned = np.array(parr_rebinned)
+        if len(Exp_days) > len(P_rebinned):
+            Exp_days = list(Exp_days) 
+            Exp_days.pop(len(Exp_days)-1)
+        Probs_rebinned.append(P_rebinned)
+    return Probs_rebinned, np.array(Exp_days)
 
 def PLPH_Plotter(KalmanAnalysisDict,daysperbin=3,use_coremap=True):
     '''Plots the first PL and PH distribution in the analysis results dictionary'''
@@ -25,7 +25,8 @@ def PLPH_Plotter(KalmanAnalysisDict,daysperbin=3,use_coremap=True):
     PL_dist = np.array(KalmanAnalysisDict["PL_distributions"][0])
     Exp_days = np.arange(1,KalmanAnalysisDict["schedule_dict"]["TOTAL_RUN"]+1,daysperbin)
     #First, we rebin the PL and PH distributions
-    PL_rebinned,PH_rebinned,Exp_days = Rebin_PLPH(PL_dist, PH_dist,Exp_days, daysperbin=daysperbin)
+    probarr,Exp_days = Rebin_PLPHs([PL_dist, PH_dist],Exp_days, daysperbin=daysperbin)
+    [PL_rebinned,PH_rebinned] = probarr
     print("LEN EXP DAYS: " + str(len(Exp_days)))
     print("LEN PH_DIST: " + str(len(PH_rebinned)))
     sns.set_style("whitegrid")
@@ -70,8 +71,10 @@ def PLPH_Spread(KalmanAnalysisDict,daysperbin=7,use_coremap=True):
     PL_stdev = np.std(PL_dist, axis=0)
     Exp_days = np.arange(1,KalmanAnalysisDict["schedule_dict"]["TOTAL_RUN"]+1,daysperbin)
     #First, we rebin the PL and PH distributions
-    PL_avgreb,PH_avgreb,Exp_days = Rebin_PLPH(PL_average, PH_average,Exp_days, daysperbin=daysperbin)
-    PL_stdreb,PH_stdreb,Exp_days = Rebin_PLPH(PL_stdev, PH_stdev,Exp_days, daysperbin=daysperbin)
+    parr1,Exp_days = Rebin_PLPHs([PL_average, PH_average],Exp_days, daysperbin=daysperbin)
+    parr2,Exp_days = Rebin_PLPHs([PL_stdev, PH_stdev],Exp_days, daysperbin=daysperbin)
+    [PL_avgreb, PH_avgreb] = parr1
+    [PL_stdreb, PH_stdreb] = parr2
     print("LEN EXP DAYS: " + str(len(Exp_days)))
     print("LEN PH_AVERAGEREBINNED: " + str(len(PH_avgreb)))
     print("LEN PH_STDEVREBINNED: " + str(len(PH_stdreb)))
@@ -106,6 +109,178 @@ def PLPH_Spread(KalmanAnalysisDict,daysperbin=7,use_coremap=True):
     #plt.legend(loc=5)
     plt.show()
 
+def _ErrBars_PLPH_Spread_FC(PH_dist, PH_average, CL):
+    PH_90hi = []
+    PH_90lo = []
+    binedges = np.arange(0.0,1.0 + (0.9/30), (1.0/30.0))
+    for i in xrange(len(PH_dist[0])): #Gets ith index of each day
+        dayprobs = []
+        for e in xrange(len(PH_dist)):
+            dayprobs.append(PH_dist[e][i])
+        dayprobs = np.array(dayprobs)
+        hist, binedges = np.histogram(dayprobs,bins=binedges)
+        #find the bin index where this average is located
+        avgind = np.where(PH_average[i] < binedges)[0][0]
+        #avgind is the index in hist that has the average value
+        #Now, we move left and right, summing up the % of events we have.
+        #Once we cross 90%CL, our bounds are defined by the edges of these
+        #Bins.
+        sumlength = np.max([avgind, len(hist)- avgind])
+        current_CL = 0.0
+        tot_entries = float(hist.sum())
+        past_CL = False
+        summedalllo = False
+        summedallhi = False 
+        for i in xrange(sumlength):
+            if past_CL is True:
+                outind = i
+                break
+            if i==0:
+                current_CL += float(hist[avgind])/ tot_entries
+            else:
+                ##add the bin on ith side of prob. average
+                #FIXME: Confirm you have overcoverage here
+                if avgind-i >= 0:
+                    current_CL += float(hist[avgind-i])/ tot_entries
+                else:
+                    summedalllo = True
+                if avgind+i <= len(hist)-1:
+                    current_CL += float(hist[avgind+i])/ tot_entries
+                else:
+                    summedallhi = True
+            if current_CL >= CL:
+                passed_CL = True
+                break
+        if summedallhi is True:
+            PH_90hi.append(binedges[len(binedges)-1])
+        else:
+            PH_90hi.append(binedges[avgind+i])
+        if summedalllo is True:
+            PH_90lo.append(binedges[0])
+        else:
+            PH_90lo.append(binedges[avgind-i])
+    return PH_90hi, PH_90lo
+
+def PLPH_Spread_FC(KalmanAnalysisDict,daysperbin=7,use_coremap=True,CL=0.90):
+    '''Plots the average and 90% CL bands (with overcoverage) of PH distribution for all
+    simulated experiments in the given results dictionary'''
+    PH_dist = np.array(KalmanAnalysisDict["PH_distributions"])
+    numexpts = len(PH_dist)
+    PH_average = np.average(PH_dist, axis=0)
+    #Now, we need to calculate the 90% CL range for each day
+    PH_90hi, PH_90lo = _ErrBars_PLPH_Spread_FC(PH_dist, PH_average, CL)
+    Exp_days = np.arange(1,KalmanAnalysisDict["schedule_dict"]["TOTAL_RUN"]+1,daysperbin)
+    #First, we rebin the PL and PH distributions
+    parr,Exp_days = Rebin_PLPHs([PH_average,PH_90hi, PH_90lo],
+            Exp_days, daysperbin=daysperbin)
+    [PH_avgreb, PH_90hireb, PH_90loreb] = parr
+    print("LEN EXP DAYS: " + str(len(Exp_days)))
+    print("LEN PH_AVERAGEREBINNED: " + str(len(PH_avgreb)))
+    sns.set_style("whitegrid")
+    sns.axes_style("whitegrid")
+    xkcd_colors = ['green','black', 'grass']
+    sns.set_palette(sns.xkcd_palette(xkcd_colors))#,len(allclasssacs)))
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    #Plot the PL days
+    asymm_error = np.array([PH_90loreb, PH_90hireb])
+    print(asymm_error)
+    ax.plot(Exp_days, PH_avgreb, alpha=0.8, linewidth=5,
+            label="P_high")
+    print(PH_90loreb)
+    if use_coremap is True:
+        try:
+            fulldays = np.arange(1,KalmanAnalysisDict["schedule_dict"]["TOTAL_RUN"]+1,1)
+            coremap = KalmanAnalysisDict['known_numcoreson']
+            max_coremap = np.max(coremap)
+            ax.plot(fulldays,np.array(coremap)/float(max_coremap), 
+                    linewidth=2, label="Core states") 
+        except KeyError:
+            print("No core map generated/saved in data dictionary.")
+            pass
+    ax.vlines(Exp_days, PH_90loreb, PH_90hireb, color='g',alpha=0.8)
+    #ax.plot(Exp_days,PL_rebinned, alpha=0.8,linewidth=4,label="P_low")
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0 , box.width*0.9, box.height])
+    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    plt.title("Prediction spread of 'both cores on' probability in %i experiments to %f CL\n"%(numexpts,CL)+\
+            "Core state legend: 1=both cores on, 0.5=one core off",fontsize=32)
+    plt.tick_params(labelsize=26)
+    plt.xlabel("Day in experiment",fontsize=30)
+    plt.ylabel("Probability of state",fontsize=30)
+    #plt.legend(loc=5)
+    plt.show()
+
+def _findOpRegions(coremap, PH_90hi, PH_90lo):
+    '''Returns the high and low bands consistent with "both cores on" and
+    "one core off" to the given CL'''
+    bothon_CLhi, bothon_CLlo = [], []
+    oneoff_CLhi, oneoff_CLlo = [], []
+    for j,state in enumerate(coremap):
+        if state == 2:
+            bothon_CLhi.append(PH_90hi[j])
+            bothon_CLlo.append(PH_90lo[j])
+        elif state == 1:
+            oneoff_CLhi.append(PH_90hi[j])
+            oneoff_CLlo.append(PH_90lo[j])
+        else:
+            print("Finding OP regions for more than the 'both cores on' and "+\
+                    "'one core off' states is not supported currently.")
+            return
+    bothon_hiavg = np.average(bothon_CLhi)
+    bothon_loavg = np.average(bothon_CLlo)
+    oneoff_hiavg = np.average(oneoff_CLhi)
+    oneoff_loavg = np.average(oneoff_CLlo)
+    return bothon_hiavg, bothon_loavg, oneoff_hiavg, oneoff_loavg 
+
+def PLPH_OpRegions(KalmanAnalysisDict,CL=0.68):
+    '''Plots the regions that the "both cores on" and "one core off" should lie
+    within to the given CL'''
+    try:
+        fulldays = np.arange(1,KalmanAnalysisDict["schedule_dict"]["TOTAL_RUN"]+1,1)
+        coremap = KalmanAnalysisDict['known_numcoreson']
+    except KeyError:
+        print("No core map generated/saved in data dictionary. Cannot run this " +\
+                "function.")
+        return
+    PH_dist = np.array(KalmanAnalysisDict["PH_distributions"])
+    numexpts = len(PH_dist)
+    PH_avg = np.average(PH_dist, axis=0)
+    #Now, we need to calculate the 90% CL range for each day
+    PH_CLhi, PH_CLlo = _ErrBars_PLPH_Spread_FC(PH_dist, PH_avg, CL)
+    Exp_days = np.arange(1,KalmanAnalysisDict["schedule_dict"]["TOTAL_RUN"]+1)
+    bothon_CLhi, bothon_CLlo, oneoff_CLhi, oneoff_CLlo = \
+            _findOpRegions(coremap, PH_CLhi, PH_CLlo)
+    print("BOTHON_LO: " + str(bothon_CLlo))
+    #First, we rebin the PL and PH distributions
+    sns.set_style("whitegrid")
+    sns.axes_style("whitegrid")
+    xkcd_colors = ['green','green', 'red', 'red']
+    sns.set_palette(sns.xkcd_palette(xkcd_colors))#,len(allclasssacs)))
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    #Plot the PL days
+    plt.title("Bands of reactor complex operational state to %f CL\n"%(CL) + \
+            "Determined using %i statistical experiments"%(numexpts))
+    plt.tick_params(labelsize=26)
+    plt.xlabel("Day in experiment",fontsize=30)
+    plt.ylabel("Probability of state",fontsize=30)
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0 , box.width*0.9, box.height])
+    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    ax.hlines(y=bothon_CLhi, xmin=Exp_days[0], xmax=Exp_days[len(Exp_days)-1], color='g',
+            label='Both on',linewidth=5,alpha=0.8)
+    ax.hlines(bothon_CLlo, Exp_days[0], Exp_days[len(Exp_days)-1], color='g',
+            linewidth=5,alpha=0.8)
+    ax.fill_between(Exp_days, bothon_CLlo, bothon_CLhi, color='g', alpha=0.2)
+    ax.hlines(oneoff_CLhi, Exp_days[0], Exp_days[len(Exp_days)-1], color='b',
+            label='One off', linewidth=5, alpha=0.8)
+    ax.hlines(oneoff_CLlo, Exp_days[0], Exp_days[len(Exp_days)-1], color='b',
+            linewidth=5, alpha=0.8)
+    ax.fill_between(Exp_days, oneoff_CLlo, oneoff_CLhi, color='b', alpha=0.2)
+    #plt.legend(loc=5)
+    plt.show()
+
 
 def OneCoreOff_CL(KalmanAnalysisDict,daysperbin=3,CL=0.683):
     '''Plots the first PL and PH distribution in the analysis results dictionary'''
@@ -120,7 +295,7 @@ def OneCoreOff_CL(KalmanAnalysisDict,daysperbin=3,CL=0.683):
     fig = plt.figure()
     ax = fig.add_subplot(1,1,1)
     #First, we rebin the PL and PH distributions
-    PL_rebinned,PH_rebinned = Rebin_PLPH(PL_dist, PH_dist,daysperbin=daysperbin)
+    [PL_rebinned,PH_rebinned], exp_days = Rebin_PLPHs([PL_dist, PH_dist],daysperbin=daysperbin)
     #Now, check if PL is >0.683 for each day or if PH > 0.683
     StateProb = []
     for measurement in PL_rebinned:
